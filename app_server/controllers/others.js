4e9d0e0be3039a15ffb38dc5a086a018c46cc664
kypertirost <liumiao1018@me.com>
2018-07-06 18:26:19 -0400

    finished chapter 4, posted to heroku

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,5 +2,8 @@
 
 /* Get About Page*/
 module.exports.about = function(req, res){
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', {
+    title: 'About',
+    about: "Loc8r was created to help people find places to sit down and get a bit of work done. \n\n This is only for the purpose of learning MEAN stack. Right now I know how to related external files to the layout and let them appear on the website. The logic is quite simple: app.js require routes; routes require controllers(.js) where res.render() function compiles your template, inserts locals there, and creates html output out of those two things."
+  });
 };
