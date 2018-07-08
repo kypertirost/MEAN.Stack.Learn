d0111178a67334607af47b7cf0eacccf15bcc75f
kypertirost <liumiao1018@me.com>
2018-07-08 18:01:05 -0400

    revise model/locations.js

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,31 +1,56 @@
 var mongoose = require ('mongoose');
 
+// deine review schema
+var reviewSchema = new mongoose.Schema({
+    author: String,
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    reviewText: String,
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
 // define openingTime schema
 var openingTimeSchema = new mongoose.Schema({
-  days: {type: String, required: true},
-  opening: String,
-  closing: String,
-  close: {type: Boolean, required: true}
-})
+    days: {
+        type: String,
+        required: true
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true
+    }
+});
 
-// define review schema
-var reviewSchema = new mongoose.Schema({
-  author: String,
-  rating:{type: Number, "defalut" = 0, min: 0, max: 5},
-  reviewText: String,
-  createdOn: {type: Date, "default": Date.now}
-})
 
 // combine the previous two into location Schema (define main schema)
 var locationSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  address: String,
-  rating: {type: Number, "defalut" = 0, min: 0, max: 5},
-  facilities: [String],
-  coords: {type: [Number], idenx: '2dsphere'},
-  openingTimes: [openingTimeSchema],
-  reviews: [reviewSchema]
+    name: {
+        type: String,
+        required: true
+    },
+    address: String,
+    rating: {
+        type: Number,
+        "default": 0,
+        min: 0,
+        max: 5
+    },
+    facilities: [String],
+    // Always store coordinates longitude, latitude order.
+    coords: {
+        type: [Number],
+        index: '2dsphere'
+    },
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
 });
-
 
 mongoose.model('Location', locationSchema);
