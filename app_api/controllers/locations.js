5f4449cf3a389120fb3a3d8fce2e119cfd40ed89
kypertirost <liumiao1018@me.com>
2018-07-10 00:52:16 -0400

    bugs fixed: auto start localhost:3000

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,27 +1,50 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJsonResponse = function (res, status, content) {
+var sendJSONresponse = function (res, status, content) {
   res.status(status);
   res.json(content)
 }
 
 module.exports.locationsCreate = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  sendJSONresponse(res, 200, {"status" : "success"});
 }
 
 module.exports.locationListByDistance = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  sendJSONresponse(res, 200, {"status" : "success"});
 }
 
-module.exports.locationsReadOne = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
-}
+module.exports.locationsReadOne = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
 
 module.exports.locationsUpdateOne = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  sendJSONresponse(res, 200, {"status" : "success"});
 }
 
 module.exports.locationsDeleteOne = function (req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  sendJSONresponse(res, 200, {"status" : "success"});
 }
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,51 +1,49 @@
-/* Define database connection. Use mongoose connection*/
 var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
-  console.log('Is in production');
-  dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGOLAB_URI;
 }
 
 mongoose.connect(dbURI);
 
-/* Listen Mongoose connection events and out put the statues*/
+// CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
-  console.log('Mongoose connected to' + dbURI);
+    console.log('Mongoose connected to ' + dbURI);
 });
 mongoose.connection.on('error', function(err) {
-  console.log('Mongoose connection error:' + err);
+    console.log('Mongoose connection error: ' + err);
 });
-mongoose.connection.on('disconnected', function(){
-  console.log('Mongoose disconnected');
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
 });
 
-/* Reusable function to close Mongoose connection*/
-gracefulShutdown = function (msg, callback) {
-  mongoose.connection.close(function(){
-    console.log('Mongoose disconnected through ' + msg);
-  });
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
-
-// For nodemon restart
+// For nodemon restarts
 process.once('SIGUSR2', function() {
-  gracefulShutdown('nodemon restart', function () {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
-
 // For app termination
 process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function() {
-    process.exit(0);
-  });
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
 });
-
 // For Heroku app termination
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown' , function(){
-    process.exit(0);
-  });
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
 
+// BRING IN YOUR SCHEMAS & MODELS
 require('./locations');
